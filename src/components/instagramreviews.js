7e00db4e5dd44b2ef85ac8282ab826eafb585439
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2024-02-01 14:14:48 -0800

    Created As Seen On Instagram banner

---
+++ src/components/instagramreviews.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import {Carousel} from "react-responsive-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const InstagramReviews = () => {
+  return(
+  <>
+    <div className="flex flex-col max-w-56 mx-24 my-28 flex-auto">
+      <p className="font-serif text-xl font-bold">As seen on instagram</p>
+      <p className="text-xs text-center mx-2">Tag #usesweettooth to be featured</p>
+    </div>
+
+  </>
+  );
+};
+
+export default InstagramReviews;
